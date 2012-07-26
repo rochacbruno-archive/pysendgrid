4e22d2172d7261d264f69f976f6a7a531b835207
Bruno Rocha <rochacbruno@gmail.com>
2012-07-26 18:14:08 -0300

    refactor

--- sendgrid.py
+++ sendgrid.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 
+import datetime
 import requests
 import json
 
@@ -30,6 +31,10 @@ class SendGrid(object):
             "schedule": {  # schedule to send
                 "add": "/api/newsletter/schedule/add.json",
                 "get": "/api/newsletter/schedule/get.json"},
+            "identity": {  # identities for newsletter
+                "add": "/api/newsletter/identity/add.json",
+                "list": "/api/newsletter/identity/list.json",
+                "get": "/api/newsletter/identity/get.json"},
         }
 
     def build_params(self, d=None):
@@ -38,22 +43,82 @@ class SendGrid(object):
         params.update(d)
         return params
 
-    def get_newsletter(self, name):
-        url = self.url_base + self.api_urls['newsletter']['get']
-        request = requests.get(url, params=self.build_params({"name": name}))
+    def build_url(self, api, resource):
+        try:
+            return self.url_base + self.api_urls[api][resource]
+        except KeyError:
+            raise("url not found for %s api and %s resource" % (api, resource))
+
+    def call(self, api, resource, params=None):
+        url = self.build_url(api, resource)
+        call_params = self.build_params(params or {})
+        request = requests.get(url, params=call_params)
         if request.status_code == 200:
-            d = json.loads(request.content)
-            return d
+            return json.loads(request.content)
+        else:
+            return request
+
+    def get_newsletter(self, name):
+        return self.call('newsletter', 'get', {"name": name})
 
     def list_newsletter(self, name=None):
-        url = self.url_base + self.api_urls['newsletter']['list']
-        if name:
-            params = self.build_params({"name": name})
-        else:
-            params = self.build_params()
+        return self.call('newsletter', 'list', {"name": name} if name else {})
 
-        request = requests.get(url, params=params)
-        if request.status_code == 200:
-            return request.content
+    def add_newsletter(self, name, subject, html, text=None, identity=None):
+        if not identity:
+            try:
+                identity = self.list_identity()[0]['identity']
+            except Exception:
+                raise("You have to inform the identity name")
+        text = text or html  # TODO: clean HTML tags
+        d = dict(identity=identity,
+                 name=name,
+                 subject=subject,
+                 text=text,
+                 html=html)
+        return self.call('newsletter', 'add', d)
+
+    def clone_newsletter(self, existing_name, new_name):
+        existing = self.get_newsletter(existing_name)
+        if isinstance(existing, dict):
+            new = self.add_newsletter(
+                name=new_name,
+                subject=existing['subject'],
+                html=existing['html'],
+                text=existing['text'],
+                identity=existing['identity']
+                )
+            return new
+        return existing
+
+    def list_identity(self, name=None):
+        return self.call('identity', 'list', {"name": name} if name else {})
+
+    def add_list(self, name):
+        return self.call('lists', 'add', {"list": name})
+
+    def get_list(self, name):
+        return self.call('lists', 'get', {"list": name} if name else {})
+
+    def add_email_to(self, list_name, **fields):
+        data = json.dumps(fields)
+        return self.call('email', 'add', dict(list=list_name, data=data))
+
+    def add_emails_to(self, list_name, emails):
+        pass
+
+    def get_email(self, list_name, **fields):
+        return self.call('email', 'get', dict(list=list_name, **fields))
+
+    def add_recipients(self, newsletter_name, list_name):
+        return self.call('recipients', 'add', {"name": newsletter_name, "list": list_name})
+
+    def add_schedule(self, newsletter_name, at=None, after=None):
+        timeformat = "%Y-%m-%d %H:%M:%S"
+        if at:
+            d = dict(name=newsletter_name, at=at.strftime(timeformat))
+        elif after:
+            d = dict(name=newsletter_name, after=after)
         else:
-            return request.error
+            d = dict(name=newsletter_name)
+        return self.call('schedule', 'add', d)
